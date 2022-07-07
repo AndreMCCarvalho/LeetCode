f1bf3b02f4efdc0ef67524ade8c981c16fdad01e
Andre Carvalho <andre.carvalho@guardsquare.com>
2022-07-07 07:32:03 +0200

    Implement strStr

---
+++ LeetCode/src/main/java/one/strStr.java
@@ -0,0 +1,27 @@
+package one;
+
+public class strStr
+{
+
+    public static void main(String[] args)
+    {
+        new strStr().strStr("mississippi", "issip");
+    }
+
+    public int strStr(String haystack, String needle) {
+        if(haystack.length() == 0) return -1;
+        if(needle.length() == 0) return 0;
+
+        for(int i = 0; i < haystack.length(); i++){
+            if(i + needle.length() > haystack.length()) break;
+
+            for(int j = 0; j < needle.length(); j++){
+                if(haystack.charAt(i+j) != needle.charAt(j)) break;
+                if (j == needle.length() - 1) return j;
+            }
+        }
+
+        return -1;
+    }
+
+}
