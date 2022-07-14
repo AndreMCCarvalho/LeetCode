071bf306e3802e85b66e6cc09efe95a8545f8aae
Andre Carvalho <andre.carvalho@guardsquare.com>
2022-07-14 09:27:26 +0200

    Climbing Stairs Alternate Solution

--- LeetCode/src/main/java/one/ClimbingStairs.java
+++ LeetCode/src/main/java/one/ClimbingStairs.java
@@ -26,4 +26,16 @@ public class ClimbingStairs
         return all_ways;
     }
 
+    public int climbStair(int n) {
+        int[] memory = new int[n + 1];
+        memory[0] = 1;
+        memory[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            memory[i] = memory[i - 1] + memory[i - 2];
+        }
+
+        return memory[n];
+    }
+
 }
