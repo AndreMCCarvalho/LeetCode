608af6666c69db27e82159648888e9aa1935ccb2
Andre Carvalho <andre.carvalho@guardsquare.com>
2022-07-27 08:14:43 +0200

    SameTree and Isolation Of Tree Class

---
+++ LeetCode/src/main/java/one/SameTree.java
@@ -0,0 +1,28 @@
+package one;
+
+import java.util.*;
+
+public class SameTree
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q != null) return false;
+        if(p != null && q == null) return false;
+        if(p == null && q == null) return true;
+        else if(p.val != q.val){
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
+
+
+//[1,2]
+//[1,null,2]
