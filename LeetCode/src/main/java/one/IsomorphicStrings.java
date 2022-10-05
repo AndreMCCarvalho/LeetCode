ddcb194e7efb51a969269d138676ebc737873f5c
Andre Carvalho <andre.carvalho@guardsquare.com>
2022-10-05 14:35:13 +0200

    IsomorphicStrings

---
+++ LeetCode/src/main/java/one/IsomorphicStrings.java
@@ -0,0 +1,28 @@
+package one;
+
+import java.util.*;
+
+public class IsomorphicStrings {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isIsomorphic(String s, String t){
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Character> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(!map.containsKey(s.charAt(i)) && !map.containsValue(t.charAt(i))){
+                map.put(s.charAt(i), t.charAt(i));
+            }
+            else{
+                if (map.get(s.charAt(i)) != t.charAt(i)) return false;
+            }
+        }
+
+        return true;
+    }
+
+}
