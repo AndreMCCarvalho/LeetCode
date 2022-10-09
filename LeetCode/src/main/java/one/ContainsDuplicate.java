026e659b4a5d4b430a9354eb5cca5a7de7563728
Andre <andremcdecarvalho@gmail.com>
2022-10-09 10:43:13 +0200

    Contains Duplicate

---
+++ LeetCode/src/main/java/one/ContainsDuplicate.java
@@ -0,0 +1,34 @@
+package main.java.one;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        new ContainsDuplicate().containsDuplicate(new int[]{1,2,3,1});
+    }
+
+    public boolean containsDuplicate(int[] nums){
+        if (nums == null || nums.length == 1) return false;
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1]) return true;
+        }
+
+        return false;
+    }
+
+    public boolean containsDuplicate(int[] nums){
+        Set<Integer> distinct = new HashSet<Integer>();
+
+        for(int i : nums){
+            if(distinct.contains(i)) return true;
+            distinct.add(i);
+        }
+
+        return false;
+    }
+
+}
