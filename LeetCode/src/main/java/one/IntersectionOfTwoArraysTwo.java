30dbf87eb7c261eeca71153f5fd9564bd93335fc
Andre Carvalho <andre.carvalho@guardsquare.com>
2022-12-13 14:29:08 +0100

    Intersection Of Two Arrays 2

--- LeetCode/src/main/java/one/IntersectionOfTwoArraysTwo.java
+++ LeetCode/src/main/java/one/IntersectionOfTwoArraysTwo.java
@@ -4,27 +4,34 @@ import java.util.*;
 
 public class IntersectionOfTwoArraysTwo {
 
+    public static void main(String[] args)
+    {
+        new IntersectionOfTwoArraysTwo().intersect(new int[]{1,2,2,1}, new int[]{2,2});
+    }
+
     public int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
 
-        ArrayList<Integer> intersect = new ArrayList<>();
 
-        int i = 0;
+        int i = 0, j = 0;
 
-        while (i < nums1.length){
-            int j = 0;
-            while (j < nums2.length){
-                if (nums1[i] == nums2[j] && !intersect.contains(nums1[i])){
-                    intersect.add(nums1[i]);
-                    break;
-                }
+        List<Integer> intersect = new ArrayList<>();
+
+        while(i < nums1.length && j < nums2.length){
+            if (nums1[i] == nums2[j]){
+                intersect.add(nums1[i]);
+                i++;
                 j++;
             }
-            i++;
+            else if (nums1[i] < nums2[j]){
+                i++;
+            }
+            else if (nums1[i] > nums2[j]){
+                j++;
+            }
         }
-        return intersect.stream()
-                        .mapToInt(Integer::intValue).toArray();
+        return intersect.stream().mapToInt(Integer::intValue).toArray();
     }
 
 }
