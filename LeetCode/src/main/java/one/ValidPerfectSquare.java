b734ff97b0b1e2fbc79a731aba0201aabfd43434
Andre <andremcdecarvalho@gmail.com>
2022-12-19 11:53:56 +0000

    Valid Perfect Square

---
+++ LeetCode/src/main/java/one/ValidPerfectSquare.java
@@ -0,0 +1,17 @@
+package main.java.one;
+
+public class ValidPerfectSquare {
+
+    public boolean isPerfectSquare(int num){
+
+       int i = 1;
+
+       while (num > 0){
+           num -= i;
+           i += 2;
+       }
+
+        return num == 0;
+    }
+
+}
